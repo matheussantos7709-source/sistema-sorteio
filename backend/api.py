ed014341e594ab6170fa157df6996cd852f97e77
Matheus Santos <matheussantos7709@gmail.com>
2026-02-10 23:34:59 -0300

    Endereço

--- backend/api.py
+++ backend/api.py
@@ -36,7 +36,8 @@ print("DB em uso:", caminho_db())
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+    allow_origins=[
+    "http://127.0.0.1:5173","http://localhost:5173","https://sistema-sorteio-1.onrender.com"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
